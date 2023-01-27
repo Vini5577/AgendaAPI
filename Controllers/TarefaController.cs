082ff0bb48722cac8acbf22a669c66b0d9b5803b
Vini5577 <ugopessoa@gmail.com>
2023-01-27 17:08:02 -0300

    finalizando tarefas

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -74,6 +74,8 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
+            _context.Add(tarefa);
+            _context.SaveChanges();
             // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
             return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
         }
